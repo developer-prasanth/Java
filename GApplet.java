87b6e1377754df05e73fc8458b9a76979bd46ad9
developer-prasanth <prasanthgamerpro@gmail.com>
2025-11-06 19:47:00 +0530

    Add files via upload
    sudo update-alternatives --config javac

---
+++ GApplet.java
@@ -0,0 +1,15 @@
+import java.applet.*;
+import java.awt.*;
+
+public class GApplet extends Applet {
+    public void paint(Graphics g) {
+        g.drawLine(50, 50, 500, 50);
+        g.drawRect(50, 100, 200, 100);
+        g.fillRect(300, 100, 200, 100);
+        g.drawRoundRect(50, 250, 200, 100, 20, 20);
+        g.fillRoundRect(300, 250, 200, 100, 20, 20);
+        g.drawOval(50, 400, 200, 100);
+        g.fillOval(300, 400, 200, 100);
+
+    }
+}
